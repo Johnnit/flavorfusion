f674c819f1be17c6c3e3480b6e795cf8460c2e20
Ajala John <150349868+Johnnit@users.noreply.github.com>
2025-01-09 16:47:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,350 @@
+let yoruba = [
+  {
+    name: 'Pounded yam and Egusi',
+    imgUrl: './Egusi-1.png',
+    description: 'Lorem xjxkxn xjxjxbx xjdhxkx djdixnd djdid didod disjs sidjdbxjx jdidb djdhdhhdi dkdkd',
+    price: 2500
+  },
+  {
+    name: 'Eba and Okro soup',
+    imgUrl: './okro_soup_recipe.jpg',
+    description: 'Lorem xjxkxn xjxjxbx xjdhxkx djdixnd djdid didod disjs sidjdbxjx jdidb djdhdhhdi dkdkd',
+    price: 2000
+  },
+  {
+    name: 'Semo and Egusi',
+    imgUrl: './WhatsApp-Image-2024-09-20-at-16.06.22.jpeg',
+    description: 'Lorem xjxkxn xjxjxbx xjdhxkx djdixnd djdid didod disjs sidjdbxjx jdidb djdhdhhdi dkdkd',
+    price: 3500
+  },
+  {
+    name: 'Pounded yam and Egusi + 3 meat',
+    imgUrl: './eba79017-4055-44d4-a4fc-d90b50f07b79.jpeg',
+    description: 'Lorem xjxkxn xjxjxbx xjdhxkx djdixnd djdid didod disjs sidjdbxjx jdidb djdhdhhdi dkdkd',
+    price: 500
+  },
+  {
+  name: 'Abula and ewedu + 3 meats',
+  imgUrl: './abula.png',
+  description: 'Lorem xjxkxn xjxjxbx xjdhxkx djdixnd djdid didod disjs sidjdbxjx jdidb djdhdhhdi dkdkd',
+  price: 7000
+},
+  {
+    name: 'Snail peppersoup',
+    imgUrl: './a36245fb-689f-4ba7-be5e-3908326e035d.jpeg',
+    description: 'Lorem xjxkxn xjxjxbx xjdhxkx djdixnd djdid didod disjs sidjdbxjx jdidb djdhdhhdi dkdkd',
+    price: 1000
+  }
+  ]
+  
+ let igbo = [
+    {
+    name: 'Akpu',
+    imgUrl: './akpu.jpg',
+    description: 'Lorem xjxkxn xjxjxbx xjdhxkx djdixnd djdid didod disjs sidjdbxjx jdidb djdhdhhdi dkdkd',
+    price: 300
+  },
+  {
+  name: 'Snail peppersoup',
+  imgUrl: './a36245fb-689f-4ba7-be5e-3908326e035d.jpeg',
+  description: 'Lorem xjxkxn xjxjxbx xjdhxkx djdixnd djdid didod disjs sidjdbxjx jdidb djdhdhhdi dkdkd',
+  price: 1000
+},
+  {
+    name: 'Eba and Okro soup',
+    imgUrl: './okro_soup_recipe.jpg',
+    description: 'It is the most popular and commonly consumed Igbo food. Consumed with different type of soup.',
+    price: 2000
+  },
+{
+  name: 'Ji Mmiri Oku',
+  imgUrl: './download (1).jpeg',
+  description: 'It is a yam and dried fish pepper soup porridge native to the Igbos of Nigeria. It is a traditional meal that is usually prepared for new mothers, right after putting to bed.',
+  price: 1000
+},
+{
+  name: 'Abacha',
+  imgUrl: './Nigerian-Abacha-and-Ugba.jpg',
+  description: 'Abacha also referred to as African salad is a traditional Enugu meal. Abacha can be served as an appetizer or main meal, it is mostly used to welcome visitor before serving them the main meal.',
+  price: 1000
+},
+{
+  name: 'Osikapa',
+  imgUrl: './istockphoto-860931464-612x612.jpg',
+  description: 'Osikapa, popularly known as rice is one of the many food eaten by the Igbo, Osikapa can be prepared in many way.',
+  price: 1000
+}
+    ]
+    
+ let bestselling = [
+   {
+  name: 'Abacha',
+  imgUrl: './Nigerian-Abacha-and-Ugba.jpg',
+  description: 'Abacha also referred to as African salad is a traditional Enugu meal. Abacha can be served as an appetizer or main meal, it is mostly used to welcome visitor before serving them the main meal.',
+  price: 1000
+},
+  {
+  name: 'Snail peppersoup',
+  imgUrl: './a36245fb-689f-4ba7-be5e-3908326e035d.jpeg',
+  description: 'Lorem xjxkxn xjxjxbx xjdhxkx djdixnd djdid didod disjs sidjdbxjx jdidb djdhdhhdi dkdkd',
+  price: 1000
+},
+  {
+    name: 'Pounded yam and Egusi',
+    imgUrl: './Egusi-1.png',
+    description: 'Lorem xjxkxn xjxjxbx xjdhxkx djdixnd djdid didod disjs sidjdbxjx jdidb djdhdhhdi dkdkd',
+    price: 2500
+  },
+  {
+    name: 'Eba and Okro soup',
+    imgUrl: './okro_soup_recipe.jpg',
+    description: 'Lorem xjxkxn xjxjxbx xjdhxkx djdixnd djdid didod disjs sidjdbxjx jdidb djdhdhhdi dkdkd',
+    price: 2000
+  },
+  {
+    name: 'Semo and Egusi',
+    imgUrl: './WhatsApp-Image-2024-09-20-at-16.06.22.jpeg',
+    description: 'Lorem xjxkxn xjxjxbx xjdhxkx djdixnd djdid didod disjs sidjdbxjx jdidb djdhdhhdi dkdkd',
+    price: 3500
+  }
+   ]
+    
+let hausa = [
+  {
+  name: 'Danwake',
+  imgUrl: './WhatsApp-Image-2024-09-20-at-16.06.26.jpeg',
+  description: 'It’s made from cowpea flour, which is shaped into dumplings and boiled. While the dumplings themselves have a mild flavour.',
+  price: 1200
+},
+{
+  name: 'Gurasa and Kuli',
+  imgUrl: './WhatsApp-Image-2024-09-20-at-16.06.23.jpeg',
+  description: 'It is a regular bread but has a lighter dough. This bread can be eaten at any time',
+  price: 1700
+},
+{
+  name: 'Dambun Shinkafa',
+  imgUrl: './WhatsApp-Image-2024-09-20-at-16.06.31.jpeg',
+  description: 'It’s a nutritious and flavorful dish that showcases the versatility of local ingredients.',
+  price: 1500
+},
+{
+  name: 'Masa',
+  imgUrl: './WhatsApp-Image-2024-09-20-at-16.06.28.jpeg',
+  description: 'This dish is made by fermenting a mixture of rice and sugar, which is then ground into a smooth batter.',
+  price: 1500
+},
+{
+  name: 'Tuwon Shinkafa',
+  imgUrl: './WhatsApp-Image-2024-09-20-at-16.06.22.jpeg',
+  description: 'It is a thick pudding that is typically served with a variety of soups like Miyar Kuka, Miyar Kubewa, and Miyar Taushe.',
+  price: 2500
+},
+{
+  name: 'Shinkafa da Wake',
+  imgUrl: './WhatsApp-Image-2024-09-20-at-16.06.21.jpeg',
+  description: ' The rice and beans are typically cooked separately until tender, then mixed together, often with added onions, tomatoes, and spices for extra flavour.',
+  price: 2000
+}
+  ]
+   
+ let english = [
+   {
+  name: 'Hot-Dogs',
+  imgUrl: './Basic-Hot-Dogs-960x640.jpg',
+  description: 'It is a thick pudding that is typically served with a variety of soups like Miyar Kuka, Miyar Kubewa, and Miyar Taushe.',
+  price: 1500
+},
+{
+  name: 'Hamburger',
+  imgUrl: './close-up-hamburger.webp',
+  description: 'It is a thick pudding that is typically served with a variety of soups like Miyar Kuka, Miyar Kubewa, and Miyar Taushe.',
+  price: 1000
+},
+{
+  name: 'Jollofrice + 2 crocker fish',
+  imgUrl: './IMG_2827.jpeg',
+  description: 'It is a thick pudding that is typically served with a variety of soups like Miyar Kuka, Miyar Kubewa, and Miyar Taushe.',
+  price: 1000
+},
+{
+  name: 'lettuce-salad-recipe',
+  imgUrl: './lettuce-salad-recipe.jpg',
+  description: 'It is a thick pudding that is typically served with a variety of soups like Miyar Kuka, Miyar Kubewa, and Miyar Taushe.',
+  price: 1000
+},
+{
+  name: 'Pizza',
+  imgUrl: './pizza.webp',
+  description: 'It is a thick pudding that is typically served with a variety of soups like Miyar Kuka, Miyar Kubewa, and Miyar Taushe.',
+  price: 1000
+}
+   ]
+   
+ let allCat = [...yoruba, ...igbo, ...hausa, ...english];
+   
+   let reviews = [
+     {
+       name: 'Sophia K.',
+       content: "FlavourFusion Co is my new go-to spot! The staff is friendly, the atmosphere is cozy, and the food is incredible. Try the Nigerian loval foods - it's to die for!",
+       ratings: 5
+     },
+     {
+       name: 'Rachel T.',
+       content: "FlavourFusion Co is a hidden gem! The chef's table experience was unforgettable. We got to watch the chefs prepare our meal and it was like a culinary show. Can't wait to come back!",
+       ratings: 3
+     },
+          {
+       name: 'Mark Z.',
+       content: "I was a bit skeptical about fusion food, but FlavourFusion Co won me over. The Pizza was surprisingly good. Prices are a bit steep, but worth it. And quick delivery.",
+       ratings: 4
+     },
+          {
+       name: 'David L.',
+       content: "I'm a big foodie and FlavourFusion Co did not disappoint. The sushi burrito was a game-changer. Only deducted a star because the wait was a bit long.",
+       ratings: 3
+     },
+     {
+       name: 'Emily R.',
+       content: "Wow, just wow! The flavors at FlavourFusion Co are out of this world! I had the hamburger and they were amazing. Service was top-notch too!",
+       ratings: 4
+     },
+     
+     ]
+   let sta;
+  document.querySelectorAll('nav ul li').forEach((e)=>{
+    e.addEventListener('click', ()=>{
+      document.querySelector('nav ul').classList.remove("show");
+    })
+  })
+   document.querySelector('.all').onclick = () => {
+     document.querySelector('.hausa').classList.remove('active');
+     document.querySelector('.english').classList.remove('active');
+     document.querySelector('.igbo').classList.remove('active');
+     document.querySelector('.yoruba').classList.remove('active');
+  addProduct(allCat);
+}
+//auto add foodlost to all section
+addProduct(allCat);
+//add youruba foods
+  document.querySelector('.yoruba').onclick = ()=>{
+    document.querySelector('.hausa').classList.remove('active');
+    document.querySelector('.english').classList.remove('active');
+    document.querySelector('.all').classList.remove('active');
+    document.querySelector('.igbo').classList.remove('active');
+    addProduct(yoruba);
+  }
+ //add igbo foods
+  document.querySelector('.igbo').onclick = ()=>{
+    document.querySelector('.hausa').classList.remove('active');
+    document.querySelector('.english').classList.remove('active');
+    document.querySelector('.all').classList.remove('active');
+     document.querySelector('.yoruba').classList.remove('active');
+    addProduct(igbo);
+  }
+  //add hausa foods
+    document.querySelector('.hausa').onclick = () => {
+      document.querySelector('.english').classList.remove('active');
+      document.querySelector('.all').classList.remove('active');
+       document.querySelector('.igbo').classList.remove('active');
+       document.querySelector('.yoruba').classList.remove('active');
+    addProduct(hausa);
+  }
+//add english food
+  document.querySelector('.english').onclick = () => {
+    document.querySelector('.hausa').classList.remove('active');
+    document.querySelector('.all').classList.remove('active');
+     document.querySelector('.igbo').classList.remove('active');
+     document.querySelector('.yoruba').classList.remove('active');
+    addProduct(english);
+  }
+  
+  /*add products*/
+  function addProduct(product){
+    document.querySelector(".foodList").innerHTML = '';
+    product.sort(() => Math.random() - 0.5);
+    for(let i = 0; i < product.length; i++){
+      
+      
+     if(screen.width <= 680){
+       if(i === 6){
+       break;
+}
+     }
+      document.querySelector(".foodList").innerHTML += `
+      <div class="foodWrap"data-aos="fade-in"data-aos-delay="300">
+            <div class="image">
+            <img src="${product[i].imgUrl}" alt="Abacha">
+          </div>
+              <div class="foodDetail">
+            <h2 class="foodname">${product[i].name}</h2>
+            <p class="description">${product[i].description} </p>
+            <div class="foodBtns">
+            <span class="price">#${product[i].price}</span>
+            <span class="order">Order Now</span>
+          </div>
+          
+          </div>
+          
+      `;
+    
+      
+    }
+  }
+
+function adProduct(product){
+    document.querySelector(".bstList").innerHTML = '';
+    product.sort(() => Math.random() - 0.5);
+    for(let i = 0; i < product.length; i++){
+      
+      
+     if(screen.width <= 680){
+       if(i === 6){
+       break;
+}
+     }
+      document.querySelector(".bstList").innerHTML += `
+      <div class="foodWrap" data-aos="fade-right"data-aos-delay="300">
+            <div class="image">
+            <img src="${product[i].imgUrl}" alt="Abacha">
+          </div>
+              <div class="foodDetail">
+            <h2 class="foodname">${product[i].name}</h2>
+            <p class="description">${product[i].description} </p>
+            <div class="foodBtns">
+            <span class="price">#${product[i].price}</span>
+            <span class="order">Order Now</span>
+          </div>
+          
+          </div>
+          
+      `;
+    
+      
+    }
+  }
+  adProduct(bestselling);
+  
+  function explore(){
+    location.href ='#menuList';
+  }
+  
+  loadReview();
+  
+function loadReview(){
+  for(let i = 0; i < reviews.length;i++){
+  document.querySelector(".cards").innerHTML += `
+        <div class="card" data-aos='fade-right'>
+        
+        <div class="stars">
+           <span class="bx bxs-star"></span>
+          <span class="bx bxs-star"></span>
+          <span class="bx bxs-star"></span>
+          <span class="bx bxs-star"></span>
+        </div>
+        <p>${reviews[i].content}</p>
+        <h1>${reviews[i].name}</h1>
+      </div>
+  `;
+}
+}
